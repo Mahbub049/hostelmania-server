eda51e269bd764c085babe8051548f2769c2023a
Muhammad Mahbub Sarwar Shafi <mahbubsarwar49@gmail.com>
2024-06-05 00:28:14 +0600

    update role api created

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@cluster0.h0zb1dz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -95,6 +95,18 @@ async function run() {
         admin = user?.role === 'admin';
       }
       res.send({ admin });
+    })
+
+    app.patch('/users/admin/:id', verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
     })
 
     // Send a ping to confirm a successful connection
