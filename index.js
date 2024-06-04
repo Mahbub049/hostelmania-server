c5461719f0011e0ce149ed2c6ad7138f09606ee4
Muhammad Mahbub Sarwar Shafi <mahbubsarwar49@gmail.com>
2024-06-05 00:01:08 +0600

    users data stored in  the db and get

--- index.js
+++ index.js
@@ -23,6 +23,26 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const userCollection = client.db("hostelmaniaDB").collection("users");
+
+
+    //User related apis
+    app.get('/users', async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email }
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: 'user already exists', insertedId: null })
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
