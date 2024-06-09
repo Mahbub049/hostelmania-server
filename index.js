5c18cb819477cca51e6ff0d9f759f213087c58df
Muhammad Mahbub Sarwar Shafi <mahbubsarwar49@gmail.com>
2024-06-09 19:30:53 +0600

    user review functionality implemented

--- index.js
+++ index.js
@@ -210,6 +210,20 @@ async function run() {
       res.send(result);
     })
 
+    app.patch('/myreviews/:id', async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+      console.log(data, id);
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          review: data.review
+        }
+      }
+      const result = await reviewCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
+
     app.delete('/review/:id', verifyToken, verifyAdmin, async(req, res)=>{
       const id = req.params.id;
       const query = {_id: new ObjectId(id)};
