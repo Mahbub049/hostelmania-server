1e6c6970ff63099645a458bc016b0c6d12242582
Muhammad Mahbub Sarwar Shafi <mahbubsarwar49@gmail.com>
2024-06-10 05:56:15 +0600

    payment intent created

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const app = express();
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -315,6 +316,25 @@ async function run() {
       const result = await upcomingCollection.deleteOne(query);
       res.send(result);
     })
+
+
+    //PAYMENT APIs
+    //Payment Intent
+    app.post('/create-payment-intent', async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+      console.log(amount, 'amount inside the intent')
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
+    });
 
 
     // Send a ping to confirm a successful connection
